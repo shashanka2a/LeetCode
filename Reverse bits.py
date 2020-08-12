2e142f028d558982d9f4151046f728ea9d650ea9
Shashank Jagannatham <37143174+shashanka2a@users.noreply.github.com>
2020-08-12 22:12:40 +0530

    Create Reverse bits.py

---
+++ Reverse bits.py
@@ -0,0 +1,16 @@
+"""
+Reverse bits of a given 32 bits unsigned integer.
+
+Example 1:
+
+Input: 00000010100101000001111010011100
+Output: 00111001011110000010100101000000
+Explanation: The input binary string 00000010100101000001111010011100 represents the unsigned integer 43261596, so return 964176192 which its binary representation is 00111001011110000010100101000000.
+"""
+
+def reverseBits(self, n):
+    ans = 0
+    for i in xrange(32):
+        ans = (ans << 1) | (n & 1)
+        n >>= 1
+    return ans

f4258483d3db88d1a5c5db5b373697a9c0b69138
Shashank Jagannatham <37143174+shashanka2a@users.noreply.github.com>
2020-08-12 21:39:22 +0530

    Create Two-Sum.py

---
+++ Two-Sum.py
@@ -0,0 +1,24 @@
+"""
+
+Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+Example:
+
+Given nums = [2, 7, 11, 15], target = 9,
+
+Because nums[0] + nums[1] = 2 + 7 = 9,
+return [0, 1].
+
+"""
+
+def twoSum(self, nums: List[int], target: int) -> List[int]:
+    #Store num:index as key:val pair
+    d = {}
+    for i,n in enumerate(nums):
+        if n in d:
+            return [d[n],i]
+        d[target - n] = i
+    #if target not found return empty indices
+    return []

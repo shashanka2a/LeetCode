853807b66e31074985534efe134753409d4c294e
Shashank Jagannatham <37143174+shashanka2a@users.noreply.github.com>
2020-08-12 22:36:22 +0530

    Create isHappy.py

---
+++ isHappy.py
@@ -0,0 +1,46 @@
+"""
+Write an algorithm to determine if a number n is "happy".
+
+A happy number is a number defined by the following process: Starting with any positive integer, replace the number by the sum of the squares of its digits, and repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1. Those numbers for which this process ends in 1 are happy numbers.
+
+Return True if n is a happy number, and False if not.
+
+Example: 
+
+Input: 19
+Output: true
+Explanation: 
+12 + 92 = 82
+82 + 22 = 68
+62 + 82 = 100
+12 + 02 + 02 = 1
+"""
+
+
+
+
+def isHappy(self, n):
+    """
+    :type n: int
+    :rtype: bool
+    """
+    def count(n):
+        c = 0
+        while n:
+            c+=1
+            n/=10
+        return c
+    def sol(n):
+        if count(n)==1:
+            return n
+        s = 0
+        while n:
+            d = n%10
+            s+=d**2
+            n/=10
+        return sol(s)
+    ns  = str(n)
+    dp = [False]*(10)
+    for i in range(1,10):
+        dp[i] = sol(i**2)==1
+    return sol(n)==1 or dp[sol(n)]

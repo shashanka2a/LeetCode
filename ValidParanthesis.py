052e2533502a91e44e78a03675ccd16ca92698c1
Shashank Jagannatham <37143174+shashanka2a@users.noreply.github.com>
2020-08-13 12:03:32 +0530

    Create ValidParanthesis.py

---
+++ ValidParanthesis.py
@@ -0,0 +1,30 @@
+"""
+Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+Note that an empty string is also considered valid.
+
+Example 1:
+
+Input: "()"
+Output: true
+Example 2:
+
+Input: "()[]{}"
+Output: true
+"""
+
+
+
+def isValid(self, s: str) -> bool:
+    d = {'(':')','[':']','{':'}'}
+    stack = []
+    for char in s:
+        if char in d:
+            stack.append(char)
+        elif len(stack)==0 or d[stack.pop()]!=char:
+            return False
+    return len(stack)==0

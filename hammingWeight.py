b8aaf8858db0005729fe4d5963830efba6eeca92
Shashank Jagannatham <37143174+shashanka2a@users.noreply.github.com>
2020-08-23 09:52:39 +0530

    Update and rename Number of 1 Bits.py to hammingWeight.py

--- hammingWeight.py
+++ hammingWeight.py
@@ -9,10 +9,6 @@ Explanation: The input binary string 00000000000000000000000000001011 has a tota
 
 """
 def hammingWeight(self, n):
-    """
-    :type n: int
-    :rtype: int
-    """
     c = 0
     for i in range(32):
         if((1<<i)&n):
